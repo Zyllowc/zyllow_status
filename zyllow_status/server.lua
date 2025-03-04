e3501e9179d6ec1407ec3d17b507402a8a2dc134
Zyllowc <124933975+Zyllowc@users.noreply.github.com>
2025-03-04 20:14:08 +0100

    Update server.lua

--- zyllow_status/server.lua
+++ zyllow_status/server.lua
@@ -1,7 +1,18 @@
+local playerPings = {}
+
 RegisterServerEvent('zyllow_ui:requestPing')
 AddEventHandler('zyllow_ui:requestPing', function()
     local src = source
-    local ping = GetPlayerPing(src)
+    local ping = playerPings[src] or GetPlayerPing(src)
     
     TriggerClientEvent('zyllow_ui:sendPing', src, ping)
+end)
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(5000)
+        for _, playerId in ipairs(GetPlayers()) do
+            playerPings[playerId] = GetPlayerPing(playerId)
+        end
+    end
 end)
